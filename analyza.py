8540d7fab638f90612ea178f201f573d4e34e17f
DampAdam <brand.adam@email.cz>
2022-06-04 02:05:07 +0200

    novy

--- analyza.py
+++ analyza.py
@@ -23,11 +23,6 @@ activity_dictionary = {
 }
 activity_dictionary_keys = activity_dictionary.keys()
 
-def getUser(alias):
-    
-    
-    return
-
 def getClass():
     
     return
@@ -60,13 +55,11 @@ def Compare(datas, co):
     n = datas[co].value_counts(normalize=True)
     return n
 
-def Mereno(hr):
-    return hr!=255
-
 # Vrátí data bez hr=255
 def GetMereny(datas):
-    """Vrátí data bez "hr"=255"""
-    return datas[Mereno(datas["hr"])]
+    """Vrátí data mezi 1-254"""
+    prom = "hr" if "hr" in datas.columns.values else "HEART_RATE"
+    return datas.query(f"{prom} > 0 and {prom} < 255")
 
 def Vypis(datas):
     print(datas.to_markdown())
@@ -75,9 +68,16 @@ def PrelozActivity(datas):
     datas["ACTIVITY"] = datas["RAW_KIND"].map(lambda x: activity_dictionary[x] if x in activity_dictionary_keys else "unknown")
     return datas
 
+def getUserByAlias(alias):
+    
+    data = pd.read_sql_query(f"SELECT DEVICE.ALIAS, MI_BAND_ACTIVITY_SAMPLE.RAW_INTENSITY, MI_BAND_ACTIVITY_SAMPLE.STEPS, MI_BAND_ACTIVITY_SAMPLE.HEART_RATE FROM MI_BAND_ACTIVITY_SAMPLE JOIN DEVICE ON MI_BAND_ACTIVITY_SAMPLE.DEVICE_ID=DEVICE._id WHERE DEVICE.ALIAS='{alias}'", con)
+    return GetMereny(data)
+
+print(getUserByAlias("Band 01"))
+
 # Get all data, kde neni tep 255, groupni by user a napis sum + mean hr
 #result = GetData(GetMereny(GetData(data, sloupce=["user", "hr", "steps"])), podle=["user"], metody=[sum, mean], co=["hr", "steps"])
 
-print(PrelozActivity(data))
+#print(PrelozActivity(data))
 
-print(Compare(data, "RAW_KIND"))
+#print(Compare(data, "RAW_KIND"))
