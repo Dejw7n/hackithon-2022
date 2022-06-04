6d28b42402267e43713f88ba7be34bcdc3a88360
DampAdam <brand.adam@email.cz>
2022-06-04 05:01:35 +0200

    novy 3

--- analyza.py
+++ analyza.py
@@ -7,11 +7,11 @@ from xmlrpc.client import DateTime
 import pandas as pd
 import sqlite3
 import datetime
-import flask
 
 con = sqlite3.connect("hackithon-2022/data/Gadgetbridge.sqlite")
 data = pd.read_sql_query("SELECT * FROM MI_BAND_ACTIVITY_SAMPLE", con)
 info = pd.read_csv("hackithon-2022/data/info.csv")
+schedule = pd.read_csv("hackithon-2022/data/schedule.csv")
 
 activity_dictionary = {
     "Running" : [98, 50, 66, 82],
@@ -26,11 +26,6 @@ activity_dictionary = {
     "NotWorn_FaceUp" : [83],
     "NotWorn_FaceDown" : [115]
 }
-
-def getClass():
-    
-    return
-
 def GetData(datas, sloupce = data.columns.values, podle = None, metody = None, co = None):
     """
         datas = jaká data
@@ -54,9 +49,7 @@ def GetData(datas, sloupce = data.columns.values, podle = None, metody = None, c
     return sl
 
 def Compare(datas, co):
-    al = datas.count()
-    n = datas[co].value_counts(normalize=True)
-    return n
+    return datas[co].value_counts(normalize=True).mul(100).round(1).astype(str) + '%'
 
 def GetMereny(datas):
     """Vrátí data mezi hr = 1-254"""
@@ -72,27 +65,9 @@ def GetDifferenceSeconds(datas):
 
 def GetWorn(datas):
     minTime = GetDifferenceSeconds(datas)
-    #VYFILTROVAT ZÁZNAMY S VERIFINUTÝM HEART RATEM, KTERÝ JE VĚTŠÍ NEŽ MINTIME
-    new = GetMereny(datas)
-    print(new.groupby("DEVICE_ID").count())
-    new["COUNT"] = new.groupby("DEVICE_ID").size()["DATETIME"]
-    return new
-
-def Vypis(datas):
-    print(datas.to_markdown())
-
-def GetActivity(x):
-    for activity in activity_dictionary:
-        if x in activity_dictionary[activity]:
-            return activity
-    return "Unknown"
-
-def PrelozActivity(datas):
-    datas["ACTIVITY"] = datas["RAW_KIND"].map(GetActivity)
-    return datas
+    return GetMereny(datas).groupby("DEVICE_ID").filter(lambda x: len(x) > minTime)
 
 def GetUserByAlias(alias):
-
     data = pd.read_sql_query(f"SELECT DEVICE.ALIAS, MI_BAND_ACTIVITY_SAMPLE.RAW_INTENSITY, MI_BAND_ACTIVITY_SAMPLE.STEPS, MI_BAND_ACTIVITY_SAMPLE.HEART_RATE FROM MI_BAND_ACTIVITY_SAMPLE JOIN DEVICE ON MI_BAND_ACTIVITY_SAMPLE.DEVICE_ID=DEVICE._id WHERE DEVICE.ALIAS='{alias}'", con)
     user_Info = info[info["alias"] == alias]
     df = GetMereny(data)
@@ -106,22 +81,23 @@ def GetUserReport(alias):
     datas = pd.read_sql_query(f"SELECT * FROM MI_BAND_ACTIVITY_SAMPLE JOIN DEVICE ON MI_BAND_ACTIVITY_SAMPLE.DEVICE_ID=DEVICE._id JOIN BATTERY ON DEVICE._id=BATTERY.DEVICE_ID", con)
     GetData(datas, sloupce=[""])
 
+def GetClassReport():
+    return schedule
+
+def GetActivity(x):
+    for activity in activity_dictionary:
+        if x in activity_dictionary[activity]:
+            return activity
+    return "Unknown"
+
+def PrelozActivity(datas):
+    datas["ACTIVITY"] = datas["RAW_KIND"].map(GetActivity)
+    return datas
+
 def PrelozDatum(datas):
     datas["TIMESTAMP"] = datas["TIMESTAMP"].apply(lambda x: datetime.datetime.fromtimestamp(x))
     datas["PASMO"] = datas["TIMESTAMP"].dt.tz_localize("UTC").dt.tz_convert("CET")
     return datas
 
-#print(GetUserByAlias("Band 01"))
-
-# Get all data, kde neni tep 255, groupni by user a napis sum + mean hr
-#result = GetData(GetMereny(GetData(data, sloupce=["user", "hr", "steps"])), podle=["user"], metody=[sum, mean], co=["hr", "steps"])
-
-#print(PrelozActivity(data))
-
-#print(Compare(data, "RAW_KIND"))
-
-#print(PrelozDatum(data))
-
-print(GetDifferenceSeconds(data))
-
-print(GetWorn(data))
+def Vypis(datas):
+    print(datas.to_markdown())
