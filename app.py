b156759eb54afd1ee52f746338b8c46a3ecb5f56
Matav <verner.matej@gmail.com>
2022-06-04 09:51:44 +0200

    První graf

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-import sqlite3
+import json
+import funkce
+import komponenty
+import plotly
+import plotly.express as px
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("base.html", page="")
+    getAVG = funkce.GetAvgDayStepsForClass() 
+    getAVG = getAVG.rename(columns={"STEPS":"Počet kroků"})
+    print(str(getAVG))
+    stepsGraph = px.bar(getAVG,labels=dict(DAY="DEN", STEPS="Počet kroků", value="Denní průměr kroků", variable=""))
+    graphJSON = json.dumps(stepsGraph, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("pages/home/home.html",stepsGraph = graphJSON)
 
 
 @app.route('/class/')
 def classRoom():
-    return render_template('base.html', page="class")
+    komponenty.getTimeTable()
+    return render_template('pages/class/class.html')
+    
 
 
 if(__name__ == "__main__"):
